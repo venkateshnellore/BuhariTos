b017afbeaad8706df56d941a00fe7affb63dbd34
padmanabhanvj9 <padmanabhanvj9@gmail.com>
2019-04-03 01:52:08 +0530

    request,cart completed

--- src/pages/cart/cart.ts
+++ src/pages/cart/cart.ts
@@ -14,12 +14,63 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'cart.html',
 })
 export class CartPage {
-
+  public item_count = 1;
+  public total = 0;
+  public tax = 30;
+  items = [
+    {
+      imageUrl: 'assets/imgs/veg.png',
+      title: 'Chicken Fried Rice',
+      rate: 140,
+      count: this.item_count,
+      itemtotal: 140,
+    },
+    {
+      imageUrl: 'assets/imgs/veg.png',
+      title: 'Mutton Biriyani',
+      rate: 240,
+      count: this.item_count,
+      itemtotal: 240,
+    },
+    {
+      imageUrl: 'assets/imgs/veg.png',
+      title: 'Fish Biriyani',
+      rate: 320,
+      count: this.item_count,
+      itemtotal: 320,
+    },
+  ];
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
+    for(var i=0;i<this.items.length;i++){
+      this.total = this.total + this.items[i].itemtotal;
+    }
     console.log('ionViewDidLoad CartPage');
   }
 
+  delete(item) {
+    alert('Deleted ' + item.title);
+  }
+
+  public removefromcart:any=[];
+  public item_price:any;
+  reduce(position,item,array){
+    if(item.count != 1 ){
+      this.items[position].count = item.count-1;
+      this.items[position].itemtotal = item.rate * item.count;
+      this.total = this.total - this.items[position].rate;
+    }
+    else{
+      this.total = this.total - this.items[position].rate;
+      array.splice(position,1);
+    }
+  }
+  add(position,item,array){
+    this.item_price = item.rate;
+    this.items[position].count = item.count +1;
+    this.items[position].itemtotal = item.rate * item.count;
+    this.total = this.total + this.items[position].rate;
+  }
 }
