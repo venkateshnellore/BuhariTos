5c6d3e451b9f3e62ce060941418ce1107ed44279
venkateshn692 <venkateshn692@gmail.com>
2019-04-15 20:17:54 +0530

    new cheanges

--- src/pages/itemlist/itemlist.ts
+++ src/pages/itemlist/itemlist.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular'; 
 
 import {DescriptionpagePage} from '../descriptionpage/descriptionpage';
-
+import { Storage } from '@ionic/storage';
 /**
  * Generated class for the ItemlistPage page.
  *
@@ -20,19 +20,63 @@ export class ItemlistPage {
    public itemlist:any=[];
    public items:any=[];
    public item_category;
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+   public buttonClicked: boolean = false;
+  public addbutton: boolean = true;
+
+  public item_price;
+  public item_count = 0;
+  public cartItemsss:any;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,public storage: Storage) {
        this.itemlist = this.navParams.get("itemlist");
       //  console.log("item listsss",this.itemlist);
        this.items = this.itemlist.item;
        console.log("items",this.items);
        this.item_category = this.itemlist.categry_name;
        console.log("item_catagory",this.item_category);
+       for (var i = 0; i < this.items.length; i++) {
+        this.items[i].item_count = 0;
+        this.items[i].add = this.addbutton;
+        this.items[i].added = this.buttonClicked;
 
+      }
+      console.log("itemmmmssssss",this.items);
+          
     }
 
+
+
+    // ionViewWillEnter() {
+     
+    // }
   ionViewDidLoad() {
     console.log('ionViewDidLoad ItemlistPage');
     console.log("soups",this.soups);
+  }
+
+  public onButtonClick(position, items, array) {
+    array[position].item_count = this.item_count + 1;
+    this.storage.get('cartdata').then((val: any) => {
+      if (val) {
+        this.cartItemsss = val;
+        this.cartItemsss.push(items)
+        this.storage.set("cartdata", this.cartItemsss);
+        console.log("Items in Cart*******************", JSON.stringify(this.cartItemsss));
+      } else {
+        this.cartItemsss = [];
+        this.cartItemsss.push(items);
+        this.storage.set("cartdata", this.cartItemsss);
+      }
+    })
+    if (items.added == false) {
+      items.added = !items.added;
+      items.add = false;
+      console.log('iffff')
+    } else {
+      items.add = true;
+      console.log('elseeeee')
+    }
   }
 
   navdescription(param){
